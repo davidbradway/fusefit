eafdba18114326cc5220913eda71238101ec618e
David Bradway <dpb6@duke.edu>
2016-08-15 13:55:32 -0400

    only allow tcx uploads

--- webui/app/__init__.py
+++ webui/app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 # This is the path to the upload directory
 UPLOAD_FOLDER = 'tmp/'
 # These are the extension that we are accepting to be uploaded
-ALLOWED_EXTENSIONS = set(['tcx','jpg'])
+ALLOWED_EXTENSIONS = set(['tcx'])
 
 # Initialize the Flask application
 app = Flask(__name__)
