c0adad47bcc3b2edc9ffea8809e16263f6a91c23
David Bradway <dpb6@duke.edu>
2016-08-16 15:47:27 -0400

    remove trailing slash on upload dir

--- webui/app/__init__.py
+++ webui/app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 
 # This is the path to the upload directory
-UPLOAD_FOLDER = 'tmp/'
+UPLOAD_FOLDER = 'tmp'
 # These are the extension that we are accepting to be uploaded
 ALLOWED_EXTENSIONS = set(['tcx'])
 
 # Initialize the Flask application
 app = Flask(__name__)
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
 
